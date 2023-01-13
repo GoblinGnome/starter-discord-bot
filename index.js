0eede7351cd9728eae92c2453d87b73c71581c87
djshdadoz <djshdadoz@gmail.com>
2023-01-13 18:04:04 -0500

    add bot code

--- index.js
+++ index.js
@@ -1,113 +1,124 @@
-
-// const { clientId, guildId, token, publicKey } = require('./config.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const cron = require("cron");
 require('dotenv').config()
-const APPLICATION_ID = process.env.APPLICATION_ID 
-const TOKEN = process.env.TOKEN 
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
 
+const TOKEN = process.env.TOKEN
 
-const axios = require('axios')
-const express = require('express');
-const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
+let scheduledMessage = new cron.CronJob('01 00 00 * * *', () => {
+      // This runs every day at 10:30:00, you can do anything you want
+      // Specifing your guild (server) and your channel
+         
+         var now = new Date().getTime();
 
+		  // Find the distance between now and the count down date
+		  var distance = deathDate - now;
 
-const app = express();
-// app.use(bodyParser.json());
-
-const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
-  timeout: 3000,
-  headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${TOKEN}`
-  }
+		  // Time calculations for days, hours, minutes and seconds
+		  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		message.channel.send(' <@114518315937955840>');
+		message.channel.send("Soroush Mahdavi has " + (days - 1) + " left to live")
+        });
+		
+client.once('ready', () => {
+    console.log('RIP');
+	scheduledMessage.start();
 });
 
+prefix = "$";
 
 
+currentDate = new Date();
+deathDate = new Date("August 14, 2025 15:34:00");
 
-app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
-  const interaction = req.body;
-
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(interaction.data.name)
-    if(interaction.data.name == 'yo'){
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `Yo ${interaction.member.user.username}!`,
-        },
-      });
+client.on('message', message => {
+	
+	
+	if (message.author.bot) return;
+	
+    daisy = message.content.toLowerCase();
+    
+    if (daisy[0] != '$')
+    {
+        return;
     }
 
-    if(interaction.data.name == 'dm'){
-      // https://discord.com/developers/docs/resources/user#create-dm
-      let c = (await discord_api.post(`/users/@me/channels`,{
-        recipient_id: interaction.member.user.id
-      })).data
-      try{
-        // https://discord.com/developers/docs/resources/channel#create-message
-        let res = await discord_api.post(`/channels/${c.id}/messages`,{
-          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
-        })
-        console.log(res.data)
-      }catch(e){
-        console.log(e)
-      }
+    daisy = daisy.substring(1);
+	
+	
+	let scheduledMessage = new cron.CronJob('01 00 00 * * *', () => {
+      // This runs every day at 10:30:00, you can do anything you want
+      // Specifing your guild (server) and your channel
+         
+         var now = new Date().getTime();
 
-      return res.send({
-        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data:{
-          content:'👍'
-        }
-      });
+		  // Find the distance between now and the count down date
+		  var distance = deathDate - now;
+
+		  // Time calculations for days, hours, minutes and seconds
+		  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		message.channel.send(' <@114518315937955840>');
+		message.channel.send("Soroush Mahdavi has " + (days - 1) + " left to live")
+        });
+		
+	
+	if (daisy == "start-countdown"){
+		scheduledMessage.start()
+		console.log("countdown started")
+	}
+	
+	if (daisy == "countdown" || daisy == "cd")
+	{
+		var now = new Date().getTime();
+
+		  // Find the distance between now and the count down date
+		  var distance = deathDate - now;
+
+		  // Time calculations for days, hours, minutes and seconds
+		  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		message.channel.send("Soroush Mahdavi will kill himself in " + days + " days " + hours + " hours " + minutes  + " minutes " + seconds + " seconds ");
     }
-  }
-
+if (daisy == "death" || daisy == "when")
+	{
+		message.channel.send("Soroush Mahdavi will commit suicide on August 14, 2025 at 3:34 pm")
+	}
+		
+	
+	
+	
+	
+	client.user.setActivity('Reaping');
 });
 
+function calcTime(currDate, deathDate)
+{
+	 return (Math.abs(currDate - deathDate)/ (1000*60*60*24));
+}
+
+function deathTimer()
+{
+	var now = new Date().getTime();
+
+		  // Find the distance between now and the count down date
+		  var distance = deathDate - now;
+
+		  // Time calculations for days, hours, minutes and seconds
+		  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	message.channel.send(' <@114518315937955840>');
+	message.channel.send("Soroush Mahdavi has " + (days) + " left to live")
+}
 
 
-app.get('/register_commands', async (req,res) =>{
-  let slash_commands = [
-    {
-      "name": "yo",
-      "description": "replies with Yo!",
-      "options": []
-    },
-    {
-      "name": "dm",
-      "description": "sends user a DM",
-      "options": []
-    }
-  ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
-  }catch(e){
-    console.error(e.code)
-    console.error(e.response?.data)
-    return res.send(`${e.code} error from discord`)
-  }
-})
 
 
-app.get('/', async (req,res) =>{
-  return res.send('Follow documentation ')
-})
+motivations = ["You can do it!", "Keep your head high, king!","We love you sushi!","Its ok to feed sometimes!","Its ok to work at Shake Shack!","We're proud of you!",
+"You're not a theatre kid!","Its ok to hide your game status from us!","Don't let internet bullies get you down!","Don't kill yourself!","No, really, don't kill yourself!", 
+"It's gonna be ok!","You're worthy.","Keep it up!","You're good at Fiora!","You've got this ('this' is life)","Bloom!","Your hair looks good today!","You'll meet someone",
+"There are plenty fish in the sea","You'll get to gold, just beleive in yourself!","You're worth it, bro.","xQc knows you exist!","Your life is poggers.","You're really good at airsoft!",
+"Hang your anime posters proudly!","You can do it!","You'll make it through!","It's ok to cry","Don't forget to walk your dog.","A gamer girl girl gf is waiting for you."];
 
-
-app.listen(8999, () => {
-
-})
-
+client.login(TOKEN);
